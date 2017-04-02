57b385ecfa97a6031b4d41178a67c61b9dfcef68
Sami Salonen <ssalonen@gmail.com>
2017-04-02 20:28:08 +0300

    Fixed config test
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- bundles/binding/org.openhab.binding.modbus.test/src/test/java/org/openhab/binding/modbus/internal/ModbusBindingConfigTest.java
+++ bundles/binding/org.openhab.binding.modbus.test/src/test/java/org/openhab/binding/modbus/internal/ModbusBindingConfigTest.java
@@ -20,12 +20,21 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.openhab.binding.modbus.internal.ItemIOConnection.IOType;
 import org.openhab.core.items.Item;
+import org.openhab.core.library.items.NumberItem;
 import org.openhab.core.library.items.StringItem;
 import org.openhab.core.library.types.StringType;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
 import org.openhab.model.item.binding.BindingConfigParseException;
 
 @RunWith(Parameterized.class)
 public class ModbusBindingConfigTest {
+
+    private static List<Class<? extends Command>> STRING_COMMANDS = new StringItem("").getAcceptedCommandTypes();
+    private static List<Class<? extends State>> STRING_STATES = new StringItem("").getAcceptedDataTypes();
+
+    private static List<Class<? extends Command>> NUMBER_COMMANDS = new NumberItem("").getAcceptedCommandTypes();
+    private static List<Class<? extends State>> NUMBER_STATES = new NumberItem("").getAcceptedDataTypes();
 
     private static StringItem stringItemWithState(String name, StringType state) {
         StringItem stringItem = new StringItem(name);
@@ -39,33 +48,41 @@ public class ModbusBindingConfigTest {
                 // Simple case, one index
                 new Object[] { new StringItem("item1"), "slave1:99",
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 99, IOType.STATE, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 99, IOType.STATE, STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 99, IOType.COMMAND, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 99, IOType.COMMAND, STRING_COMMANDS, STRING_STATES) },
+                        null },
+
+                // Simple case different item type
+                new Object[] { new NumberItem("item1"), "slave1:99",
+                        new ItemIOConnection[] {
+                                new ItemIOConnection("slave1", 99, IOType.STATE, NUMBER_COMMANDS, NUMBER_STATES) },
+                        new ItemIOConnection[] {
+                                new ItemIOConnection("slave1", 99, IOType.COMMAND, NUMBER_COMMANDS, NUMBER_STATES) },
                         null },
 
                 // Simple case, one index, initial state
                 new Object[] { stringItemWithState("item1", new StringType("foobar")), "slave1:99",
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 99, IOType.STATE, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 99, IOType.STATE, STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 99, IOType.COMMAND, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 99, IOType.COMMAND, STRING_COMMANDS, STRING_STATES) },
                         null },
 
                 // Simple, read and write index different
                 new Object[] { new StringItem("item1"), "slave1:<100:>99",
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 100, IOType.STATE, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 100, IOType.STATE, STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 99, IOType.COMMAND, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 99, IOType.COMMAND, STRING_COMMANDS, STRING_STATES) },
                         null },
 
                 // Simple, read and write index different, order different
                 new Object[] { new StringItem("item1"), "slave1:>99:<100",
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 100, IOType.STATE, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 100, IOType.STATE, STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 99, IOType.COMMAND, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 99, IOType.COMMAND, STRING_COMMANDS, STRING_STATES) },
                         null },
                 // invalid, index missing
                 new Object[] { new StringItem("item1"), "slave1", null, null,
@@ -86,52 +103,51 @@ public class ModbusBindingConfigTest {
                 new Object[] { new StringItem("item2"),
                         "<[slave1:99:trigger=*,transformation=TRANSFORMATION,valueType=int32]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                new Transformation("TRANSFORMATION", null, null), "int32", Arrays.asList(),
-                                Arrays.asList()) },
+                                new Transformation("TRANSFORMATION", null, null), "int32", STRING_COMMANDS,
+                                STRING_STATES) },
 
                         new ItemIOConnection[0], null },
 
                 // extended, single read using keywords, some parameters omitted, and order different
                 new Object[] { new StringItem("item2"), "<[slave1:99:transformation=TRANSFORMATION]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "default",
-                                new Transformation("TRANSFORMATION", null, null), "default", Arrays.asList(),
-                                Arrays.asList()) },
+                                new Transformation("TRANSFORMATION", null, null), "default", STRING_COMMANDS,
+                                STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, single read, default transformation
                 new Object[] { new StringItem("item2"), "<[slave1:99:trigger=*,transformation=default,valueType=int32]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                Transformation.IDENTITY_TRANSFORMATION, "int32", Arrays.asList(), Arrays.asList()) },
+                                Transformation.IDENTITY_TRANSFORMATION, "int32", STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, single read, default transformation, case insensitive type
                 new Object[] { new StringItem("item2"), "<[slave1:99:trigger=*,transformation=default,valueType=int32]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                Transformation.IDENTITY_TRANSFORMATION, "int32", Arrays.asList(), Arrays.asList()) },
+                                Transformation.IDENTITY_TRANSFORMATION, "int32", STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, single read, valid transformation
                 new Object[] { new StringItem("item2"),
                         "<[slave1:99:trigger=*,transformation=JS(getValue.js),valueType=int32]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                new Transformation("", "JS", "getValue.js"), "int32", Arrays.asList(),
-                                Arrays.asList()) },
+                                new Transformation("", "JS", "getValue.js"), "int32", STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, two reads, valid transformations
                 new Object[] { new StringItem("item2"),
                         "<[slave1:99:trigger=*,transformation=JS(getValue.js),valueType=int32],<[slave2:98:trigger=*,transformation=FUN(getValue2.js),valueType=uint16]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                new Transformation("", "JS", "getValue.js"), "int32", Arrays.asList(), Arrays.asList()),
+                                new Transformation("", "JS", "getValue.js"), "int32", STRING_COMMANDS, STRING_STATES),
                                 new ItemIOConnection("slave2", 98, IOType.STATE, "*",
-                                        new Transformation("", "FUN", "getValue2.js"), "uint16", Arrays.asList(),
-                                        Arrays.asList()) },
+                                        new Transformation("", "FUN", "getValue2.js"), "uint16", STRING_COMMANDS,
+                                        STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, two reads, valid transformations; whitespace around comma
                 new Object[] { new StringItem("item2"),
                         "<[slave1:99:trigger=*,transformation=JS(getValue.js),valueType=int32]\t ,"
                                 + "<[slave2:98:trigger=*,transformation=\"REGEX(,.*,)\",valueType=uint16]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                new Transformation("", "JS", "getValue.js"), "int32", Arrays.asList(), Arrays.asList()),
+                                new Transformation("", "JS", "getValue.js"), "int32", STRING_COMMANDS, STRING_STATES),
                                 new ItemIOConnection("slave2", 98, IOType.STATE, "*",
-                                        new Transformation("", "REGEX", ",.*,"), "uint16", Arrays.asList(),
-                                        Arrays.asList()) },
+                                        new Transformation("", "REGEX", ",.*,"), "uint16", STRING_COMMANDS,
+                                        STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, three reads, valid transformations; whitespace around comma
                 new Object[] { new StringItem("item2"),
@@ -139,23 +155,23 @@ public class ModbusBindingConfigTest {
                                 + "<[slave2:98:trigger=*,transformation=\"REGEX(,.*,)\",valueType=uint16],"
                                 + "<[slave3:97:trigger=*,transformation=\"REGEX(,.*,)\",valueType=float32]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                new Transformation("", "JS", "getValue.js"), "int32", Arrays.asList(), Arrays.asList()),
+                                new Transformation("", "JS", "getValue.js"), "int32", STRING_COMMANDS, STRING_STATES),
                                 new ItemIOConnection("slave2", 98, IOType.STATE, "*",
-                                        new Transformation("", "REGEX", ",.*,"), "uint16", Arrays.asList(),
-                                        Arrays.asList()),
+                                        new Transformation("", "REGEX", ",.*,"), "uint16", STRING_COMMANDS,
+                                        STRING_STATES),
                                 new ItemIOConnection("slave3", 97, IOType.STATE, "*",
-                                        new Transformation("", "REGEX", ",.*,"), "float32", Arrays.asList(),
-                                        Arrays.asList()) },
+                                        new Transformation("", "REGEX", ",.*,"), "float32", STRING_COMMANDS,
+                                        STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, two reads, valid transformations; comma in the beginning,tricky transform
                 new Object[] { new StringItem("item2"),
                         ",<[slave1:99:trigger=*,transformation=JS(getValue.js),valueType=int32]\t ,"
                                 + "<[slave2:98:trigger=*,transformation=\"REGEX(,.*[],(.*))\",valueType=uint16]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                new Transformation("", "JS", "getValue.js"), "int32", Arrays.asList(), Arrays.asList()),
+                                new Transformation("", "JS", "getValue.js"), "int32", STRING_COMMANDS, STRING_STATES),
                                 new ItemIOConnection("slave2", 98, IOType.STATE, "*",
-                                        new Transformation("", "REGEX", ",.*[],(.*)"), "uint16", Arrays.asList(),
-                                        Arrays.asList()) },
+                                        new Transformation("", "REGEX", ",.*[],(.*)"), "uint16", STRING_COMMANDS,
+                                        STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, two reads, valid transformations; comma in the beginning, transform with
                 // java-quoted
@@ -163,10 +179,10 @@ public class ModbusBindingConfigTest {
                 new Object[] { new StringItem("item2"), // FIXME
                         ",<[slave1:99:trigger=*,transformation=JS(getValue.js),valueType=int32]\t  ,   <[slave2:98:trigger=*,transformation=\"REGEX(\\\",.*[:]),)\",valueType=uint16]",
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.STATE, "*",
-                                new Transformation("", "JS", "getValue.js"), "int32", Arrays.asList(), Arrays.asList()),
+                                new Transformation("", "JS", "getValue.js"), "int32", STRING_COMMANDS, STRING_STATES),
                                 new ItemIOConnection("slave2", 98, IOType.STATE, "*",
-                                        new Transformation("", "REGEX", "\",.*[:]),"), "uint16", Arrays.asList(),
-                                        Arrays.asList()) },
+                                        new Transformation("", "REGEX", "\",.*[:]),"), "uint16", STRING_COMMANDS,
+                                        STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // extended, two reads, valid transformations; comma in the beginning, transform quoted but
                 // without
@@ -181,18 +197,18 @@ public class ModbusBindingConfigTest {
                         " >[slave3:97:trigger=ThisIsTrigger,transformation=\"FOOBAR(=\\\",.*[:]),)\",valueType=float32]",
                         new ItemIOConnection[] {},
                         new ItemIOConnection[] { new ItemIOConnection("slave3", 97, IOType.COMMAND, "ThisIsTrigger",
-                                new Transformation("", "FOOBAR", "=\",.*[:]),"), "float32", Arrays.asList(),
-                                Arrays.asList()) },
+                                new Transformation("", "FOOBAR", "=\",.*[:]),"), "float32", STRING_COMMANDS,
+                                STRING_STATES) },
                         null },
                 // two writes, valid transformations; transform quoted
                 new Object[] { new StringItem("item2"),
                         ">[slave1:99:trigger=*,transformation=JS(getValue.js),valueType=int32]\t  ,   >[slave2:98:trigger=*,transformation=\"REGEX(\\\",.=*[:]),)\",valueType=uint16]",
                         new ItemIOConnection[0],
                         new ItemIOConnection[] { new ItemIOConnection("slave1", 99, IOType.COMMAND, "*",
-                                new Transformation("", "JS", "getValue.js"), "int32", Arrays.asList(), Arrays.asList()),
+                                new Transformation("", "JS", "getValue.js"), "int32", STRING_COMMANDS, STRING_STATES),
                                 new ItemIOConnection("slave2", 98, IOType.COMMAND, "*",
-                                        new Transformation("", "REGEX", "\",.=*[:]),"), "uint16", Arrays.asList(),
-                                        Arrays.asList()) },
+                                        new Transformation("", "REGEX", "\",.=*[:]),"), "uint16", STRING_COMMANDS,
+                                        STRING_STATES) },
                         null },
                 // invalid valuetype
                 new Object[] { new StringItem("item2"), "<[slave1:99:trigger=*,valueType=invalidValueType]", null, null,
@@ -212,12 +228,12 @@ public class ModbusBindingConfigTest {
                 // all defaults
                 new Object[] { new StringItem("item2"), "<[slave1:101]",
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 101, IOType.STATE, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 101, IOType.STATE, STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[0], null },
                 // all defaults, ending with colon
                 new Object[] { new StringItem("item2"), "<[slave1:101:]",
                         new ItemIOConnection[] {
-                                new ItemIOConnection("slave1", 101, IOType.STATE, Arrays.asList(), Arrays.asList()) },
+                                new ItemIOConnection("slave1", 101, IOType.STATE, STRING_COMMANDS, STRING_STATES) },
                         new ItemIOConnection[0], null }
 
         );
@@ -255,7 +271,7 @@ public class ModbusBindingConfigTest {
                 throw e;
             }
         }
-        assertThat(config.getItemClass(), is(equalTo(StringItem.class)));
+        assertThat(config.getItemClass(), is(equalTo(this.item.getClass())));
         assertThat(config.getItemName(), is(equalTo(item.getName())));
 
         assertThat(config.getWriteConnections().toArray(), is(equalTo(expectedWriteConnections)));
